aaa215a6e77f0f5a97d855523157936c4b9d79a6
andrea <andrea25cb@gmail.com>
2023-01-18 19:24:46 +0100

    editar y añadir posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 urlpatterns += [
@@ -17,8 +20,3 @@ urlpatterns += [
     path('contacto/', views.contacto, name='contacto'),
 ]
 
-# urlpatterns += [
-#     path('', views.post_list, name='post_list'),
-#     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-#     path('post/new/', views.post_new, name='post_new'),
-# ]
